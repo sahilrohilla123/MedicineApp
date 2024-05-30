47e6e316e4fc4da2e814699b61533791a98523fc
sahilrohilla123 <sr5121393@gmail.com>
2024-05-30 23:18:48 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors(
+  {
+    origin : ["https://medicine-app-685y.vercel.app"],
+    methods : ["POST" , "GET"],
+    credentials : true
+  }
+));
 app.use(express.json());
 app.get("/",(req,res) =>{
   res.json("Hello");
